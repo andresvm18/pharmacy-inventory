2e04ef769ba7ab868c8a43b4756b21e268f8be18
andresvm18 <viquezandres1811@gmail.com>
2026-07-12 11:53:52 -0600

    feat(services): add create/update/delete methods for categories and suppliers

--- client/src/services/categoryService.js
+++ client/src/services/categoryService.js
@@ -5,4 +5,18 @@ export const categoryService = {
     const response = await api.get('/categories');
     return response.data;
   },
+
+  create: async (data) => {
+    const response = await api.post('/categories', data);
+    return response.data;
+  },
+
+  update: async (id, data) => {
+    const response = await api.put(`/categories/${id}`, data);
+    return response.data;
+  },
+
+  remove: async (id) => {
+    await api.delete(`/categories/${id}`);
+  },
 };
--- client/src/services/supplierService.js
+++ client/src/services/supplierService.js
@@ -5,4 +5,18 @@ export const supplierService = {
     const response = await api.get('/suppliers');
     return response.data;
   },
+
+  create: async (data) => {
+    const response = await api.post('/suppliers', data);
+    return response.data;
+  },
+
+  update: async (id, data) => {
+    const response = await api.put(`/suppliers/${id}`, data);
+    return response.data;
+  },
+
+  remove: async (id) => {
+    await api.delete(`/suppliers/${id}`);
+  },
 };
